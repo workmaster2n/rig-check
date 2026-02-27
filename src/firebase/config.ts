d577811235b17350431482f3d31498a1b3f22b80
Tyler DeWitt <tyler.scott.dewitt@gmail.com>
2026-02-27 08:33:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9287029071-d9962",
+  "appId": "1:827646195722:web:81be2620be6c4947d747f3",
+  "apiKey": "AIzaSyBpSUTOuL7Pt3gJKk4_-lxrcV-NfmGIhWo",
+  "authDomain": "studio-9287029071-d9962.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "827646195722"
+};
